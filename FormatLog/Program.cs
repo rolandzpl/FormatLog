40d96e72952309db0729990b46b081b8d333db90
Roland Żerek <rolandz@poczta.fm>
2023-10-30 07:55:07 +0100

    Added --in/--out path parsing

--- FormatLog/Program.cs
+++ FormatLog/Program.cs
@@ -1,16 +1,36 @@
-﻿using System.CommandLine;
+using System.CommandLine;
 using Lithium.FormatLog;
 
 var inOption = new Option<TextReader?>(
     name: "--in",
-    description: "Input of the log lines"
+    description: "Input of the log lines",
+    parseArgument: result =>
+    {
+        if (result.Tokens.Count != 1)
+        {
+            result.ErrorMessage = "--in requires file path";
+            return null;
+        }
+        return new StreamReader(File.OpenRead(result.Tokens.First().Value));
+    }
 );
+inOption.AddAlias("-i");
 inOption.SetDefaultValue(Console.In);
 
 var outOption = new Option<TextWriter?>(
     name: "--out",
-    description: "Output where the log lines are to be renderred"
+    description: "Output where the log lines are to be renderred",
+    parseArgument: result =>
+    {
+        if (result.Tokens.Count != 1)
+        {
+            result.ErrorMessage = "--out requires file path";
+            return null;
+        }
+        return new StreamWriter(File.OpenWrite(result.Tokens.First().Value));
+    }
 );
+outOption.AddAlias("-o");
 outOption.SetDefaultValue(Console.Out);
 
 var rootCommand = new RootCommand("Sample app for System.CommandLine");
